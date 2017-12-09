d21fc8434ec501da68194854c7a11979293ba326
Camden Wagner <camden.i.wagner@ivarcode.net>
2017-12-09 16:16:32 -0500

    authors/header

--- app/src/main/java/jcubed/wakemewhen/MainActivity.java
+++ app/src/main/java/jcubed/wakemewhen/MainActivity.java
@@ -3,6 +3,11 @@ package jcubed.wakemewhen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*Jason Karos, Brian Ward, Camden Wagner
+* WakeMeWhen
+* 12/9/2017
+*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
