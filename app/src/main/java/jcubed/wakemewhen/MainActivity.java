958e418b7d121f39c2df983ddd8de72368be23a9
j <jskaros16@outlook.com>
2017-12-09 15:51:07 -0500

    Second commit

--- app/src/main/java/jcubed/wakemewhen/MainActivity.java
+++ app/src/main/java/jcubed/wakemewhen/MainActivity.java
@@ -3,6 +3,9 @@ package jcubed.wakemewhen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//test
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
