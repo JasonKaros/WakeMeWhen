a31eaed347c824f76178db02b070c195feaeb3d6
Brian Ward <bward3@conncoll.edu>
2017-12-18 00:34:40 -0500

    additem in optionsmenu opens map. not working: onmapclick

--- app/src/main/java/jcubed/wakemewhen/MainActivity.java
+++ app/src/main/java/jcubed/wakemewhen/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -23,6 +24,7 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity: ";
     ListView mListView;
     ArrayList<Alarm> mAlarmList;
     AlarmAdapter mAdapter;
@@ -71,14 +73,14 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
- //   @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case
-//                Intent intent = new Intent("j.cubed.booklibrary.BookActivity");
-//                intent.putExtra("id", -2);
-//                startActivityForResult(intent, BOOK_RECORD_REQUEST);
-//        }
-//        return false;
-//    }
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_alarm:
+                Intent i = new Intent(this, MapsActivity.class);
+                startActivity(i);
+        }
+        return false;
+    }
 }
