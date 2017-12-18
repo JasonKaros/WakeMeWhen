748d7731d8f77d5c1b0b711de47e8c635bd3bda8
Brian Ward <bward3@conncoll.edu>
2017-12-18 06:28:13 -0500

    what a load of changes!!
    MapActivity is now pretty much fully functional.
    -on map click it sets dest value as lat-long of click
    -search bar needs to be connected via google services
    -there are a few gradle files that need to be updated
    -to get google play services working on AVD, need to have api > 24 afaik
    -geo services api key was being wonky, not sure if I changed it from the old one, may need to tweak that...
    AlarmEdit doesn't really do much yet, but it takes in an intent from the mapactivity with lat long and sometimes address vals
    Haven't dealt with services yet, so someone should figure that out, though I doubt it'll be too bad.
    
    TODO:
    Populate alarm db in alarmedit, allow intents to access alarmedit from main menu (need to differentiate intent from mainactivity vs intent from mapsactivity, but I think that'll be easy to handle)
    
    Background location services--setting a proximity alert per alarm. Actually getting alarm to go off etc.
    
    other than that I think we're pretty close :^)
    
    time for bed for me
    
    -Bri

--- app/src/main/java/jcubed/wakemewhen/AlarmEdit.java
+++ app/src/main/java/jcubed/wakemewhen/AlarmEdit.java
@@ -1,15 +1,33 @@
 package jcubed.wakemewhen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class AlarmEdit extends AppCompatActivity {
 
     private Alarm usrAlarm;
+    private final String TAG = "AlarmEdit: ";
+    private double latitude;
+    private double longitude;
+    private String address;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_edit);
+
+        //getting lat-long doubles from the MapActivity
+        Intent intent = getIntent();
+        latitude = intent.getDoubleExtra(MapsActivity.LATITUTDE,0);
+        longitude = intent.getDoubleExtra(MapsActivity.LONGITUDE,0);
+        if (intent.hasExtra(MapsActivity.ADDRESS)) {
+            address = intent.getStringExtra(MapsActivity.ADDRESS); }
+        Log.i(TAG, "Latitude: " + String.valueOf(latitude)
+               + ", Longitude: " + String.valueOf(longitude));
     }
+
+
 }
--- app/src/main/java/jcubed/wakemewhen/MapsActivity.java
+++ app/src/main/java/jcubed/wakemewhen/MapsActivity.java
@@ -1,6 +1,7 @@
 package jcubed.wakemewhen;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -10,14 +11,23 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -27,7 +37,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     LocationManager locationManager;
     LocationListener locationListener;
     Double[] alarmLatLong = new Double[2];
+    String addressString;
     final private int REQUEST_ACCESS_CODE = 123;
+    private boolean mapReady = false;
+    private boolean destinationSet = false;
+    private boolean hasAddress = false;
+    private final MarkerOptions destMarker = new MarkerOptions();
+    static final String LATITUTDE = "latitude";
+    static final String LONGITUDE = "longitude";
+    static final String ADDRESS = "address";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +55,97 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                if (mapReady) {
+                    LatLng dest = place.getLatLng();
+                    alarmLatLong[0] = dest.latitude;
+                    alarmLatLong[1] = dest.longitude;
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dest,8));
+                    destMarker.position(dest);
+                    destMarker.title("Destination");
+                    mMap.addMarker(destMarker);
+                    destinationSet = true;
+                    if (place.getAddress() != null) {
+                        addressString = place.getAddress().toString();
+                        hasAddress = true;
+                    }
+
+                } else { Log.w(TAG, "Map not ready!"); }
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        Button doneBtn = (Button) findViewById(R.id.done_button);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!destinationSet) {
+                    CharSequence text = "Please set a destination.";
+                    Toast.makeText(MapsActivity.this, text, Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "dest not set");
+                } else {
+                    Intent i = new Intent(MapsActivity.this, AlarmEdit.class);
+                    i.putExtra(LATITUTDE, alarmLatLong[0]);
+                    i.putExtra(LONGITUDE, alarmLatLong[1]);
+                    if (hasAddress) { i.putExtra(ADDRESS, addressString); }
+                    startActivity(i);
+                }
+            }
+        });
+
+
     }
-
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mapReady = true;
         mMap = googleMap;
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationListener = new MyLocationListener();
 
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                double lat = latLng.latitude;
+                double lon = latLng.longitude;
+                LatLng dest = new LatLng(lat, lon);
+                destMarker.position(dest);
+                destMarker.title("Destination");
+                mMap.addMarker(destMarker);
+                alarmLatLong[0] = lat;
+                alarmLatLong[1] = lon;
+                destinationSet = true;
+                Log.i(TAG, "Lat: " + Double.toString(lat) + ", Lon: " + Double.toString(lon));
+            }
+        });
+
+
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_CODE);
+            ActivityCompat.requestPermissions(this,
+                    new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_CODE);
+            Log.d(TAG, "Permissions not granted.");
             return;
         } else {
             permissionGranted = true;
@@ -68,17 +154,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (permissionGranted) {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
+
+            Log.d(TAG, "Requesting location updates.");
         }
 
-        // Add a marker in Sydney and move the camera
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                double lat = latLng.latitude;
-                double lon = latLng.longitude;
-                Log.d(TAG, "Lat: " + Double.toString(lat) + ", Lon: " + Double.toString(lon));
-            }
-        });
+        //adds a marker at your current location (do we want this or not?)
+
+        Location currentLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        LatLng p = new LatLng(currentLoc.getLatitude(), currentLoc.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(p));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        MarkerOptions myMarker = new MarkerOptions();
+        myMarker.position(p);
+        myMarker.title("Current Location");
+        mMap.addMarker(myMarker);
+
     }
 
     private class MyLocationListener implements LocationListener {
@@ -87,18 +177,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 LatLng p = new LatLng((int) (loc.getLatitude()),(int) (loc.getLongitude()));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(p));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(3));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(8));
             }
         }
 
         //Called when a tracked/listened provider is disabled
         public void onProviderDisabled(String provider) {
-            Log.w(TAG, "Provider disabled.");
+            Log.d(TAG, "Provider disabled.");
         }
 
         //Called when a tracked/listened provider is enabled
         public void onProviderEnabled(String provider) {
-            Log.w(TAG, "Provider enabled.");
+            Log.d(TAG, "Provider enabled.");
         }
 
         //Called when there is a change in the provider status
@@ -114,7 +204,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 case LocationProvider.TEMPORARILY_UNAVAILABLE:
                     statusString = "TEMPORARILY UNAVAILABLE";
             }
-            Log.w(TAG, statusString);
+            Log.d(TAG, statusString);
         }
     }
 }
