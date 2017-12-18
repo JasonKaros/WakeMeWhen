2d38bbe7a08d5e154951e802a473a2bf52be94f6
j <jskaros16@outlook.com>
2017-12-17 21:01:49 -0500

    Begin DBAdapter construction, finish AlarmAdapter

--- app/src/main/java/jcubed/wakemewhen/AlarmEdit.java
+++ app/src/main/java/jcubed/wakemewhen/AlarmEdit.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class AlarmEdit extends AppCompatActivity {
 
+    private Alarm usrAlarm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
